317e91ad18eccbb15cb45edf633ba86e42df80b5
kennedymwaniki <kennedymwaniki089@gmail.com>
2024-06-19 07:21:03 +0300

    card fix

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -3,9 +3,11 @@ const ProfileCard = ({ name, bio, age, image }) => {
   return (
     <div className="card">
       <img src={image} className="img" />
-      <h1> Name: {name}</h1>
-      <p>age: {age}</p>
-      <p>Bio: {bio}</p>
+      <div className="bio">
+        <h1> Name: {name}</h1>
+        <p>age: {age}</p>
+        <p>Bio: {bio}</p>
+      </div>
     </div>
   );
 };
